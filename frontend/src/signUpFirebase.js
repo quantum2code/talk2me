c35431d118f94e68bfdf57552dc4e1052c50bec3
sagario9 <saikatsardar180@gmail.com>
2025-07-21 16:05:38 +0530

    Update signUpFirebase.js

--- frontend/src/signUpFirebase.js
+++ frontend/src/signUpFirebase.js
@@ -10,7 +10,7 @@ const firebaseConfig = {
   messagingSenderId: 'YOUR_MESSAGING_SENDER_ID',
   appId: 'YOUR_APP_ID',
 }
-
+ 
 const app = initializeApp(firebaseConfig)
 const auth = getAuth(app)
 const provider = new GoogleAuthProvider()
