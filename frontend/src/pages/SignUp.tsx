7ed88b09eb617e49047423123a322c6b070706f9
quantumCode <quantumcode22@gmail.com>
2025-10-04 19:23:07 +0530

    stash

--- frontend/src/pages/SignUp.tsx
+++ frontend/src/pages/SignUp.tsx
@@ -22,7 +22,7 @@ function Signup() {
   const signIn = async () => {
     const { data, error } = await authClient.signIn.social({
       provider: "google",
-      callbackURL: "/c/",
+      callbackURL: "http://localhost:5173/",
     });
   };
 
